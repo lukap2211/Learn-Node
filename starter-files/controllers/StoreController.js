6cf267999db5616f7aef7154055d805a8bd13baf
Luka Puharic <lukap2211@gmail.com>
2019-03-11 16:34:13 -0400

    WIP

--- starter-files/controllers/StoreController.js
+++ starter-files/controllers/StoreController.js
@@ -1,17 +1,37 @@
 const mongoose = require('mongoose')
-const Store = mongoose.module('Store')
-
+const Store = mongoose.model('Store')
 
 exports.homePage = (req, res) => {
-  console.log(req.name)
   res.render('index')
 }
 
 exports.addStore = (req, res) => {
-  res.render('editStore', {title : 'Add store'})
+  res.render('editStore', { title: 'Add store' })
 }
 
-exports.createStore = (req, res) => {
-  const store = new Store(req.body)
-  store.save()
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save()
+  req.flash('success', `Sucesfully created ${store.name}. Wanna leave a review?`)
+  res.redirect(`/store/${store.slug}`)
+}
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find()
+  res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({_id: req.params.id})
+  res.render('editStore', {'title': `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true, // return new store instead the old one
+    runValidators: true
+  }).exec()
+  req.flash('success', `Sucesfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`)
+
+
 }
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const StoreController = require('../controllers/StoreController.js')
+const { catchErrors } = require('../handlers/errorHandlers.js')
 
 // Do work here
-router.get('/', StoreController.homePage);
+router.get('/', catchErrors(StoreController.getStores));
+router.get('/stores', catchErrors(StoreController.getStores));
 router.get('/add', StoreController.addStore);
 router.post('/add', StoreController.createStore);
+router.post('/add/:id', StoreController.updateStore);
+router.get('/stores/:id/edit', catchErrors(StoreController.editStore));
 
 router.get('/reverse/:name', (req, res) => {
   console.log('NAME: ${name}')
